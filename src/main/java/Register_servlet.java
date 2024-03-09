2d0112aabd20394d64f613f9b3b32f96f8d1f9d8
amine <kendimohammedamine@gmail.com>
2024-03-09 01:20:35 +0100

    Test COMMIT

---
+++ src/main/java/Register_servlet.java
@@ -0,0 +1,74 @@
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * Servlet implementation class AA
+ */
+@WebServlet("/register")
+public class Register_servlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+
+    public Register_servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        response.getWriter().append("Served at: ").append(request.getContextPath());
+    }
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        String uname = request.getParameter("name");
+        String uemail = request.getParameter("email");
+        String upwd = request.getParameter("pass");
+        String ucontact = request.getParameter("contact");
+
+
+
+        RequestDispatcher dispatcher = null;
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/syndic_db?useSSL=false", "root", "1999");
+            PreparedStatement pst = con.prepareStatement("insert into users(u_name, u_pwd, u_email, u_mobile) values (?,?,?,?)");
+            pst.setString(1, uname);
+            pst.setString(2, upwd);
+            pst.setString(3, uemail);
+            pst.setString(4, ucontact);
+
+            int rowCount = pst.executeUpdate();
+            dispatcher = request.getRequestDispatcher("registration.jsp");
+            if (rowCount > 0) {
+                request.setAttribute("status", "success");
+            } else {
+            }
+            request.setAttribute("status", "failed");
+            dispatcher.forward(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+// TODO Auto-generated catch block e.printStackTrace();
+
+
+            }
+        }
+    }
+}
